30241540184e5fa7df4d168f8515942f2bccb91c
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2024-05-04 21:05:56 +0500

    Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,59 @@
-import React from 'react';
-import './Navbar.css';
-
+import React, { useState } from 'react';
 
 function Navbar() {
-  return (
-    <div className="navbar">
-      <div className="menu-icon">
-      <i class="gg-menu-left-alt"></i>
-      </div>
-      <div className="logo">
-      <h1 style={{ padding: 0,letterSpacing: '12px', margin: '2px', fontWeight: 'normal' }}>L   O   G   O</h1>
+  const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
 
+  const toggleOffcanvas = () => {
+    setIsOffcanvasOpen(!isOffcanvasOpen);
+  };
+
+  return (
+<div style={{
+  background: 'linear-gradient(135deg, #FCE4EC, #E0FFFF)',padding:"0px",
+}}>
+    <nav className="navbar fixed-top">
+      <div className="container-fluid">
+      <a className="navbar-brand" Link="#"></a>
+        <h1 style={{ margin: "5px",letterSpacing: '12px', margin: '2px', fontWeight: 'normal' }}>T   O   D   O</h1>
+        <button className="navbar-toggler" type="button" onClick={toggleOffcanvas} aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`offcanvas offcanvas-end ${isOffcanvasOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <button type="button" className="btn-close" onClick={toggleOffcanvas} aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" Link="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" Link="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" Link="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" Link="#">Action</a></li>
+                  <li><a className="dropdown-item" Link="#">Another action</a></li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li><a className="dropdown-item" Link="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex mt-3" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div className="search-icon">
-      <i class="gg-search" style={{top:'-3px'}}></i>
-      <img width="25px" height="25px" src="https://img.icons8.com/cotton/64/gender-neutral-user--v2.png" alt="gender-neutral-user--v2"/>
-        <img src="/Images/cart.png" title="market icons" height='25px' width="25px"></img>
-      </div>
+    </nav><br></br><br></br><br></br>
+    <hr style={{marginTop:"-2px"}}></hr>
     </div>
   );
 }
