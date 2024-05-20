5b3df6d8348371c71fcec6f19181ac89bb7967d3
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2024-05-20 14:26:33 +0500

    Revert "App.js"
    This reverts commit b1d3fe9ed0d25ad0d3706bab6d7ca7309acdc1e7.

--- src/App.js
+++ src/App.js
@@ -13,46 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           
-        </Routes>import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Home from './components/home'; 
-import Profile from './components/profile';
-import { auth } from './firebase';
-
-function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        setUser(authUser);
-      } else {
-        setUser(null);
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={user ? <Navigate to="/home" /> : <Login />} />
-        <Route path="/login" element={!user ? <Login /> : <Navigate to="/home" />} />
-        <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/home" />} />
-        <Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
-        <Route path="/profile" element={user ? <Profile /> : <Navigate to="/" />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-
+        </Routes>
       </Router>
       
     </div>
