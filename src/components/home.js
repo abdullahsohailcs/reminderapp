ab4fa867c4f54b179e7b044937340ae3281bbd04
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2024-05-20 14:26:48 +0500

    Revert "home.js"
    This reverts commit 3b4bef2b03d0a3af911993c52851dd46a1134897.

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import Navbar from './navbar';
+import Blogpost from './postblog';
 import DisplayList from './displaytask';
-
 function Home() {
+  
+
   return (
     <div style={{
         backgroundImage: 'url("/Images/cloud1.jpg")',
@@ -12,9 +13,14 @@ function Home() {
         width: '100%', // Set the width to 100%
         /* You can add more CSS properties here */
       }}>
-      <Navbar />
-      <br />
+      <Navbar /> 
+      <br>
+      </br>
       <DisplayList />
+      <p>adsssssssssss</p>
+      <p>adsssssssssss</p>
+      <p>adsssssssssss</p>
+      <p>adsssssssssss</p>
     </div>
   );
 }
